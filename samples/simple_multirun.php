84d3ef00724c2a408020ed9fa60b09b89ef8a878
gavinchi <ryan@zuttonet.com>
2011-06-19 22:46:18 -0600

    expanded documentation

--- samples/simple_multirun.php
+++ samples/simple_multirun.php
@@ -1,15 +1,17 @@
-<?php namespace rolex;
-require_once "../lib/rolex.php";
+<?php
+require_once "../rolex.php";
     
-$obj = new \stdClass;
+$obj = new stdClass;
 $obj->foo = "bar";
 $dev_null = null;
-$foo = "test";
     
-echo r::run( "instance variable access", function() use ($obj, $dev_null) {
+echo rolex\r::run( "instance variable access", function() use ($obj, $dev_null) {
     $dev_null = $obj->foo; // to test how long it takes to make the assignment
-}, 1);
+}, 10);
 
-echo r::run( "straight-up variable access", function() use ($dev_null, $foo) {
+$foo = "test";
+$dev_null = null;
+
+echo rolex\r::run( "straight-up variable access", function() use ($dev_null, $foo) {
     $dev_null = $foo;
-}, 1);
+}, 10);
